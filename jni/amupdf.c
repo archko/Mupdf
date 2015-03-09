5b5be50d4042c4b21346832b56afccb2d67eee71
archko <archko@gmail.com>
2015-03-09 15:04:13 +0800

    fix decode bug

--- jni/amupdf.c
+++ jni/amupdf.c
@@ -30,7 +30,7 @@
 #undef TIME_DISPLAY_LIST
 
 #define MAX_SEARCH_HITS (500)
-#define NUM_CACHE (1)
+#define NUM_CACHE (2)
 #define STRIKE_HEIGHT (0.375f)
 #define UNDERLINE_HEIGHT (0.075f)
 #define LINE_THICKNESS (0.07f)
@@ -876,6 +876,205 @@ JNI_FN(MuPDFCore_drawPage3)(JNIEnv *env, jobject thiz, jobject bitmap,
 	}
 
 	//LOGI("locking pixels\n");
+	if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
+		LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
+		return 0;
+	}
+
+	/* Call mupdf to render display list to screen */
+	LOGE("Rendering page(%d)=%dx%d patch=[%d,%d,%d,%d]",
+			pc->number, pageW, pageH, patchX, patchY, patchW, patchH);
+
+	fz_try(ctx)
+	{
+		fz_irect pixbbox;
+		pdf_document *idoc = pdf_specifics(ctx, doc);
+
+		//if (idoc)
+		//{
+		//	/* Update the changed-rects for both hq patch and main bitmap */
+		//	update_changed_rects(glo, pc, idoc);
+        //
+		//	/* Then drop the changed-rects for the bitmap we're about to
+		//	render because we are rendering the entire area */
+		//	drop_changed_rects(ctx, hq ? &pc->hq_changed_rects : &pc->changed_rects);
+		//}
+
+		if (pc->page_list == NULL)
+		{
+			/* Render to list */
+			pc->page_list = fz_new_display_list(ctx);
+			dev = fz_new_list_device(ctx, pc->page_list);
+			fz_run_page_contents(ctx, pc->page, dev, &fz_identity, cookie);
+			fz_drop_device(ctx, dev);
+			dev = NULL;
+			if (cookie != NULL && cookie->abort)
+			{
+				fz_drop_display_list(ctx, pc->page_list);
+				pc->page_list = NULL;
+				fz_throw(ctx, FZ_ERROR_GENERIC, "Render aborted");
+		    }
+		}
+		if (pc->annot_list == NULL)
+		{
+			fz_annot *annot;
+			pc->annot_list = fz_new_display_list(ctx);
+			dev = fz_new_list_device(ctx, pc->annot_list);
+			for (annot = fz_first_annot(ctx, pc->page); annot; annot = fz_next_annot(ctx, pc->page, annot))
+                fz_run_annot(ctx, pc->page, annot, dev, &fz_identity, cookie);
+			fz_drop_device(ctx, dev);
+			dev = NULL;
+			if (cookie != NULL && cookie->abort)
+			{
+				fz_drop_display_list(ctx, pc->annot_list);
+				pc->annot_list = NULL;
+				fz_throw(ctx, FZ_ERROR_GENERIC, "Render aborted");
+		    }
+		}
+		bbox.x0 = patchX;
+		bbox.y0 = patchY;
+		bbox.x1 = patchX + patchW;
+		bbox.y1 = patchY + patchH;
+		pixbbox = bbox;
+		pixbbox.x1 = pixbbox.x0 + info.width;
+		/* pixmaps cannot handle right-edge padding, so the bbox must be expanded to
+		 * match the pixels data */
+		pix = fz_new_pixmap_with_bbox_and_data(ctx, glo->colorspace, &pixbbox, pixels);
+		if (pc->page_list == NULL && pc->annot_list == NULL)
+		{
+			fz_clear_pixmap_with_value(ctx, pix, 0xd0);
+			break;
+		}
+		fz_clear_pixmap_with_value(ctx, pix, 0xff);
+
+		zoom = glo->resolution / 72;
+		fz_scale(&ctm, zoom, zoom);
+		rect = pc->media_box;
+		fz_round_rect(&bbox, fz_transform_rect(&rect, &ctm));
+		/* Now, adjust ctm so that it would give the correct page width
+		 * heights. */
+		xscale = (float)pageW/(float)(bbox.x1-bbox.x0);
+		yscale = (float)pageH/(float)(bbox.y1-bbox.y0);
+		fz_concat(&ctm, &ctm, fz_scale(&scale, xscale, yscale));
+		rect = pc->media_box;
+		fz_transform_rect(&rect, &ctm);
+		dev = fz_new_draw_device(ctx, pix);
+#ifdef TIME_DISPLAY_LIST
+		{
+			clock_t time;
+			int i;
+
+			LOGE("Executing display list");
+			time = clock();
+			for (i=0; i<100;i++) {
+#endif
+				if (pc->page_list)
+					fz_run_display_list(ctx, pc->page_list, dev, &ctm, &rect, cookie);
+				if (cookie != NULL && cookie->abort)
+					fz_throw(ctx, FZ_ERROR_GENERIC, "Render aborted");
+
+				if (pc->annot_list)
+					fz_run_display_list(ctx, pc->annot_list, dev, &ctm, &rect, cookie);
+				if (cookie != NULL && cookie->abort)
+					fz_throw(ctx, FZ_ERROR_GENERIC, "Render aborted");
+
+#ifdef TIME_DISPLAY_LIST
+			}
+			time = clock() - time;
+			LOGE("100 renders in %d (%d per sec)", time, CLOCKS_PER_SEC);
+		}
+#endif
+		fz_drop_device(ctx, dev);
+        dev = NULL;
+        fz_drop_pixmap(ctx, pix);
+		//LOGE("Rendered");
+	}
+	fz_always(ctx)
+	{
+		fz_drop_device(ctx, dev);
+		dev = NULL;
+	}
+	fz_catch(ctx)
+	{
+		LOGE("Render failed");
+	}
+
+	AndroidBitmap_unlockPixels(env, bitmap);
+
+	return 1;
+}
+
+JNIEXPORT jboolean JNICALL
+JNI_FN(MuPDFCore_drawPageWithNumber)(JNIEnv *env, jobject thiz, int page, jobject bitmap,
+		int pageW, int pageH, int patchX, int patchY, int patchW, int patchH, jlong cookiePtr)
+{
+	AndroidBitmapInfo info;
+    void *pixels;
+    int ret;
+    fz_device *dev = NULL;
+    float zoom;
+    fz_matrix ctm;
+    fz_irect bbox;
+    fz_rect rect;
+    fz_pixmap *pix = NULL;
+    float xscale, yscale;
+    globals *glo = get_globals(env, thiz);
+    fz_context *ctx = glo->ctx;
+    fz_document *doc = glo->doc;
+    page_cache *pc;
+	
+    int hq = (patchW < pageW || patchH < pageH);
+    fz_matrix scale;
+    fz_cookie *cookie = (fz_cookie *)(intptr_t)cookiePtr;
+	
+	//----------- load page start -----------
+	//LOGI("drawPageWithNumber start %d...", page);
+	glo->current = 0;
+	pc = &glo->pages[glo->current];
+	pc->width = 100;
+	pc->height = 100;
+
+	pc->number = page;
+	//LOGI("drawPageWithNumber Goto page %d...", page);
+	fz_try(ctx)
+	{
+		//fz_rect rect;
+		//LOGI("drawPageWithNumber Load page %d", pc->number);
+		pc->page = fz_load_page(ctx, glo->doc, pc->number);
+		zoom = glo->resolution / 72;
+		fz_bound_page(ctx, pc->page, &pc->media_box);
+		fz_scale(&ctm, zoom, zoom);
+		rect = pc->media_box;
+		fz_round_rect(&bbox, fz_transform_rect(&rect, &ctm));
+		pc->width = bbox.x1-bbox.x0;
+		pc->height = bbox.y1-bbox.y0;
+		//LOGI("drawPageWithNumber %d,%d,%d,%d", bbox.x0,bbox.y0,bbox.x1,bbox.y1);
+	}
+	fz_catch(ctx)
+	{
+		LOGE("cannot make displaylist from page %d", pc->number);
+	}
+	//----------- load page finished -----------
+
+	if (pc->page == NULL)
+		return 0;
+
+	fz_var(pix);
+	fz_var(dev);
+
+	LOGI("In native method\n");
+	if ((ret = AndroidBitmap_getInfo(env, bitmap, &info)) < 0) {
+		LOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
+		return 0;
+	}
+
+	LOGI("Checking format\n");
+	if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
+		LOGE("Bitmap format is not RGBA_8888 !");
+		return 0;
+	}
+
+	LOGI("locking pixels\n");
 	if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
 		LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
 		return 0;
