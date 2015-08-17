0e5a75df5dcd50113ca64e9f849231f0eea32a54
archko <wrf14174>
2015-08-17 17:17:49 +0800

    outline update to v21,use toolbar replace actionbar reset options

--- app/src/main/java/com/artifex/mupdfdemo/OutlineActivity.java
+++ app/src/main/java/com/artifex/mupdfdemo/OutlineActivity.java
@@ -1,14 +1,16 @@
 package com.artifex.mupdfdemo;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toolbar;
 
-public class OutlineActivity extends ListActivity {
+public class OutlineActivity extends Activity {
 	OutlineItem mItems[];
 
 	/** Called when the activity is first created. */
@@ -16,61 +18,82 @@ public class OutlineActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            getActionBar().setDisplayHomeAsUpEnabled(true);
+		setContentView(R.layout.outline_list);
+
+		if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+			Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+			//toolbar.setLogo(R.drawable.icon);
+			setActionBar(toolbar);
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+			/*toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					finish();
+				}
+			});*/
+		} else if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
             getActionBar().setDisplayShowHomeEnabled(true);
         } else {
             setTheme(R.style.AppFullscreen);
         }
 
+		final ListView listView=(ListView) findViewById(R.id.list);
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				onListItemClick((ListView) parent, view, position, id);
+			}
+		});
 		mItems = OutlineActivityData.get().items;
-		setListAdapter(new OutlineAdapter(getLayoutInflater(),mItems));
+		listView.setAdapter(new OutlineAdapter(getLayoutInflater(), mItems));
 		// Restore the position within the list from last viewing
 
 		//getListView().setDividerHeight(0);
-		getListView().setCacheColorHint(getResources().getColor(android.R.color.transparent));
+		//listView.setCacheColorHint(getResources().getColor(android.R.color.transparent));
 		setResult(-1);
-		getListView().getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+		listView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 			@Override
 			public void onGlobalLayout() {
-				getListView().getViewTreeObserver().removeGlobalOnLayoutListener(this);
-				int pos = OutlineActivityData.get().position;
-				if (null != getIntent()) {
-					int cp = getIntent().getIntExtra("cp", -1);
-					if (cp != -1) {
-						pos = cp;
-					}
-				}
-				OutlineItem[] items = OutlineActivityData.get().items;
-				//System.out.println("pos:"+pos+" items:"+items.length);
-				OutlineItem item;
-				int idx=0;
-				for (int i = items.length - 1; i >= 0; i--) {
-					item = items[i];
-					//System.out.println("pos page:"+item.page);
-					if (item.page <= pos) {
-						//pos = item.page;
-						idx=i;
-						break;
-					}
-				}
-
-				//System.out.println("pos:"+pos+" idx:"+idx);
-				if (idx < getListView().getAdapter().getCount()) {
-					getListView().setSelection(idx);
-				}
+				listView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+				setSelection(listView);
 			}
 		});
 	}
 
-	@Override
+	private void setSelection(ListView listView) {
+		int pos = OutlineActivityData.get().position;
+		if (null != getIntent()) {
+            int cp = getIntent().getIntExtra("cp", -1);
+            if (cp != -1) {
+                pos = cp;
+            }
+        }
+		OutlineItem[] items = OutlineActivityData.get().items;
+		//System.out.println("pos:"+pos+" items:"+items.length);
+		OutlineItem item;
+		int idx=0;
+		for (int i = items.length - 1; i >= 0; i--) {
+            item = items[i];
+            //System.out.println("pos page:"+item.page);
+            if (item.page <= pos) {
+                //pos = item.page;
+                idx=i;
+                break;
+            }
+        }
+
+		//System.out.println("pos:"+pos+" idx:"+idx);
+		if (idx < listView.getAdapter().getCount()) {
+            listView.setSelection(idx);
+        }
+	}
+
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
-		OutlineActivityData.get().position = getListView().getFirstVisiblePosition();
+		OutlineActivityData.get().position = l.getFirstVisiblePosition();
 		setResult(mItems[position].page);
 		finish();
 	}
-
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
